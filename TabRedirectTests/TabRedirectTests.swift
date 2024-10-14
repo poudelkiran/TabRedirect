dc792fba9c25f0c47b6c5d5bb5a554d9cab9b08b
Kiran Poudel <kiranpoudel.mail@gmail.com>
2024-10-14 20:57:17 +0545

    Initial Commit

---
+++ TabRedirectTests/TabRedirectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabRedirectTests.swift
+//  TabRedirectTests
+//
+//  Created by Kiran Poudel on 14/10/2024.
+//
+
+import Testing
+@testable import TabRedirect
+
+struct TabRedirectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
